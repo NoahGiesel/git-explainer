52700171724c4fcfa007dd2e3ee6851afe93b762
NoahGiesel <noahgiesel@gmail.com>
2020-07-06 23:57:11 +0200

    added css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
       <h2 className="title">titolo </h2>
       <div className="wrapper">
         <div className="menu">
